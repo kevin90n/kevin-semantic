6cb4f3ffd6b94d1cbb6d9fa2f827276227fd5a99
kevin90n <41412337+kevin90n@users.noreply.github.com>
2023-11-13 19:56:09 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -15,13 +15,13 @@ pr_id=$(curl -L  -X POST  https://api.github.com/repos/kevin90n/kevin-semantic/p
     -H "X-GitHub-Api-Version: 2022-11-28" \
     -d '{"title":"chore: automatic merge","body":"Please pull these awesome changes in!","head":\"'"$headbranch"'\","base":\"'"$basebranch"'\"}'  |  jq -r .node_id) || request_status=$?
 
-if [[ -z $request_status && -n $pr_id && $pr_id != null ]]
+if [[ -z ${request_status+x} && -n $pr_id && $pr_id != null ]]
 then
     enabled_at=$(curl -X POST https://api.github.com/graphql \
          -H 'Content-Type: application/json' \
          -H "Authorization: bearer $GITHUB_TOKEN" \
          -d '{"query": "mutation {enablePullRequestAutoMerge(input: {pullRequestId: \"'"$pr_id"'\"}) {pullRequest {autoMergeRequest{enabledAt}}}}"' | jq .data.enablePullRequestAutoMerge.pullRequest.autoMergeRequest.enabledAt)  || request_status=$?
-    if [[ -n $request_status && -n $enabled_at ]]
+    if [[ -z ${request_status+x} && -n $enabled_at ]]
     then
       echo "failed to enable automerge for the PR: #$pr_id"
       exit 1
